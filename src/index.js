ab012ddb9ada9ebc35419100ba93d3a4f8742681
Raquel Cruz <raquelcruz@Raquels-MacBook-Pro-2.local>
2025-03-30 21:45:56 -0400

    new files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 
 const myFirstElement = <h1>Hello React!</h1>;
 
+/* React Array Methods */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const myArray = ['apple', 'banana', 'orange'];
+
+const myList = myArray.map((item) => <p>{item}</p>);
+
+const container = document.getElementById('root');
+// changed this:
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// to this:
+const root = ReactDOM.createRoot(container);
+
 root.render(myFirstElement);
+root.render(myList);
